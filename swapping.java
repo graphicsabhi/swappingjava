ba673495ea1897036c26aa4e20789772ca3865e9
Satyam <satyamsobhraj819@gmail.com>
2022-08-20 23:56:41 +0530

    swapping two number

---
+++ swapping.java
@@ -0,0 +1,15 @@
+public class swapping {
+    public static void main(String[] args) {
+        int num1=24,num2=34;
+        // assign number1 to temp
+        int temp = num1;
+        // assign num2 to num1
+        num2=num1;
+        // assign num1 to num2
+        num2=temp;
+        
+        System.out.println("num1: " +num1);
+        System.out.println("num2: " +num2);
+
+    }
+}
